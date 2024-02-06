f00deb1f72fe77955f4f4847f76c4f90c4054311
Ari Stathopoulos <aristath@gmail.com>
2024-02-06 09:55:46 +0200

    Add stats for terms

---
+++ includes/stats/class-stat-terms.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Stats about terms.
+ */
+
+namespace ProgressPlanner\Stats;
+
+/**
+ * Stats about terms.
+ */
+class Stat_Terms extends Stat {
+
+	/**
+	 * The taxonomy for this stat.
+	 *
+	 * @var string
+	 */
+	protected $taxonomy = 'category';
+
+	/**
+	 * Set the taxonomy for this stat.
+	 *
+	 * @param string $taxonomy The taxonomy.
+	 */
+	public function set_taxonomy( $taxonomy ) {
+		$this->taxonomy = $taxonomy;
+	}
+
+	/**
+	 * Get the stat data.
+	 *
+	 * @return array
+	 */
+	public function get_data() {
+		return array(
+			'total' => (array) \wp_count_terms( [ 'taxonomy' => $this->taxonomy ] ),
+		);
+	}
+}
