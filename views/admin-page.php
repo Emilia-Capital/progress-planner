ba0a16d67f8b218feb0c009b401dce08d61a809e
Ari Stathopoulos <aristath@gmail.com>
2024-03-11 10:57:33 +0200

    Admin page layout tweaks

--- views/admin-page.php
+++ views/admin-page.php
@@ -12,32 +12,52 @@ $prpl_scan_pending = null === \progress_planner()->get_query()->get_oldest_activ
 <div class="wrap prpl-wrap">
 	<h1 class="screen-reader-text"><?php \esc_html_e( 'Progress Planner', 'progress-planner' ); ?></h1>
 	<?php require 'admin-page-header.php'; ?>
+	<?php
+	$prpl_admin_page_columns = [
+		[
+			[
+				'website-activity-score',
+				'published-content-density',
+				'published-content',
+				// 'published-pages',
+				// 'published-posts',
+				'published-words',
+			],
+		],
+		[
+			[
+				'activity-scores',
+				'plugins',
+				'badge-content',
+				'badge-streak',
+			],
+			[
+				'latest-badge',
+				'badges-progress',
+				'__filter-numbers',
+			],
+		],
+	];
+	?>
+
 
 	<?php if ( $prpl_scan_pending ) : ?>
 		<?php include 'admin-page-form-scan.php'; ?>
 	<?php else : ?>
 		<div class="prpl-widgets-container">
-			<?php
-			foreach ( [
-				'website-activity-score',
-				'activity-scores',
-				// 'latest-badge',
-				// 'published-pages',
-				// 'published-posts',
-				'published-content-density',
-				'published-content',
-				'published-words',
-				'badges-progress',
-				'badge-content',
-				'badge-streak',
-				'plugins',
-				'__filter-numbers',
-			] as $prpl_widget ) {
-				echo '<div class="prpl-widget-wrapper prpl-' . esc_attr( $prpl_widget ) . '">';
-				include "widgets/{$prpl_widget}.php";
-				echo '</div>';
-			}
-			?>
+			<?php foreach ( $prpl_admin_page_columns as $prpl_column_main ) : ?>
+				<div class="prpl-column-main">
+					<?php foreach ( $prpl_column_main as $prpl_column ) : ?>
+						<div class="prpl-column">
+							<?php foreach ( $prpl_column as $prpl_widget ) : ?>
+								<div class="prpl-widget-wrapper prpl-<?php echo esc_attr( $prpl_widget ); ?>">
+									<?php include "widgets/{$prpl_widget}.php"; ?>
+								</div>
+							<?php endforeach; ?>
+						</div>
+					<?php endforeach; ?>
+				</div>
+			<?php endforeach; ?>
 		</div>
 		<?php include 'admin-page-debug.php'; ?>
 		<hr>
