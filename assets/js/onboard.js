a7ab89e8c69310f6fc38875765265fdd8e1ed15f
Ari Stathopoulos <aristath@gmail.com>
2024-04-01 11:26:02 +0300

    split functions

--- assets/js/onboard.js
+++ assets/js/onboard.js
@@ -1,5 +1,50 @@
 /* global progressPlanner */
 
+/**
+ * Make the AJAX request.
+ *
+ * @param {Object} data The data to send with the request.
+ */
+const progressPlannerAjaxAPIRequest = ( data ) => {
+	progressPlannerAjaxRequest( {
+		method: 'POST',
+		url: progressPlanner.onboardAPIUrl,
+		data: data,
+		successAction: ( response ) => {
+			console.log( data );
+			console.log( response );
+			if ( response.success ) {
+			}
+		},
+		failAction: ( response ) => {
+			console.log( response );
+		},
+	} );
+};
+
+/**
+ * Make the AJAX request.
+ *
+ * @param {Object} data The data to send with the request.
+ */
+const progressPlannerOnboardCall = ( data ) => {
+	progressPlannerAjaxRequest( {
+		method: 'POST',
+		url: progressPlanner.onboardGetNonceURL,
+		data: data,
+		successAction: ( response ) => {
+			if ( 'ok' === response.status ) {
+
+				// Add the nonce to our data object.
+				data.nonce = response.nonce;
+console.log(data);
+				// Make the request to the API.
+				progressPlannerAjaxAPIRequest( data );
+			}
+		},
+	} );
+};
+
 if ( document.getElementById( 'prpl-onboarding-form' ) ) {
 	document.getElementById( 'prpl-onboarding-form' ).addEventListener( 'submit', function( event ) {
 		event.preventDefault();
@@ -15,33 +60,6 @@ if ( document.getElementById( 'prpl-onboarding-form' ) ) {
 
 		// Make a request to get the nonce.
 		// Once the nonce is received, make a request to the API.
-		progressPlannerAjaxRequest( {
-			method: 'POST',
-			url: progressPlanner.onboardGetNonceURL,
-			data: data,
-			successAction: ( response ) => {
-				if ( 'ok' === response.status ) {
-
-					// Add the nonce to our data object.
-					data.nonce = response.nonce;
-console.log(data);
-					// Make the request to the API.
-					progressPlannerAjaxRequest( {
-						method: 'POST',
-						url: progressPlanner.onboardAPIUrl,
-						data: data,
-						successAction: ( response ) => {
-							console.log( data );
-							console.log( response );
-							if ( response.success ) {
-							}
-						},
-						failAction: ( response ) => {
-							console.log( response );
-						},
-					} );
-				}
-			},
-		} );
+		progressPlannerOnboardCall( data );
 	} );
 }
