a0b6b08ddc23d6a8fc105ff56613abde8e4a7e1d
Ari Stathopoulos <aristath@gmail.com>
2024-04-03 09:37:25 +0300

    Add main plugin headers

--- progress-planner.php
+++ progress-planner.php
@@ -1,8 +1,20 @@
 <?php
 /**
- * Plugin name: Progress Planner
+ * A plugin to help you fight procrastination and get things done.
  *
  * @package ProgressPlanner
+ *
+ * Plugin name:       Progress Planner
+ * Plugin URI:        https://progressplanner.com/
+ * Description:       A plugin to help you fight procrastination and get things done.
+ * Requires at least: 6.2
+ * Requires PHP:      7.4
+ * Version:           1.0.0
+ * Author:            Joost de Valk
+ * Author URI:        https://joost.blog/
+ * License:           GPL-3.0+
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:       progress-planner
  */
 
 define( 'PROGRESS_PLANNER_DIR', __DIR__ );
