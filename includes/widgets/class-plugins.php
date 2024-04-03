34b51f077d5aafebe9059b12d687073f18c5481f
Ari Stathopoulos <aristath@gmail.com>
2024-04-03 14:20:30 +0300

    fix nested paragraphs

--- includes/widgets/class-plugins.php
+++ includes/widgets/class-plugins.php
@@ -44,27 +44,25 @@ final class Plugins extends Widget {
 				</span>
 			</div>
 			<div class="prpl-widget-content">
-				<p>
-					<?php if ( 0 === $pending_plugin_updates ) : ?>
-						<?php \esc_html_e( 'Well done! All your plugins are up to date.', 'progress-planner' ); ?>
-					<?php else : ?>
-						<p>
-							<?php
-							printf(
-								/* translators: %1$s: number of plugins. %2$s: Total number of pending updates. */
-								\esc_html__( 'You have %1$s plugins installed. There are %2$s pending updates.', 'progress-planner' ),
-								\esc_html( \number_format_i18n( $plugins_count ) ),
-								'<span class="accent">' . \esc_html( \number_format_i18n( $pending_plugin_updates ) ) . '</span>'
-							);
-							?>
-						</p>
-						<p>
-							<a href="<?php echo \esc_url( \admin_url( 'update-core.php' ) ); ?>" style="font-weight: 600; color:var(--prpl-color-gray-6);">
-								<?php \esc_html_e( 'Update your plugins now.', 'progress-planner' ); ?>
-							</a>
-						</p>
-					<?php endif; ?>
-				</p>
+				<?php if ( 0 === $pending_plugin_updates ) : ?>
+					<p><?php \esc_html_e( 'Well done! All your plugins are up to date.', 'progress-planner' ); ?></p>
+				<?php else : ?>
+					<p>
+						<?php
+						printf(
+							/* translators: %1$s: number of plugins. %2$s: Total number of pending updates. */
+							\esc_html__( 'You have %1$s plugins installed. There are %2$s pending updates.', 'progress-planner' ),
+							\esc_html( \number_format_i18n( $plugins_count ) ),
+							'<span class="accent">' . \esc_html( \number_format_i18n( $pending_plugin_updates ) ) . '</span>'
+						);
+						?>
+					</p>
+					<p>
+						<a href="<?php echo \esc_url( \admin_url( 'update-core.php' ) ); ?>" style="font-weight: 600; color:var(--prpl-color-gray-6);">
+							<?php \esc_html_e( 'Update your plugins now.', 'progress-planner' ); ?>
+						</a>
+					</p>
+				<?php endif; ?>
 			</div>
 		</div>
 		<?php
