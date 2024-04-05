99b6064491f068ca39f8d7cc60344c688782e703
Ari Stathopoulos <aristath@gmail.com>
2024-04-05 10:53:37 +0300

    Update progress-planner.php
    Co-authored-by: Joost de Valk <joost@altha.nl>

--- progress-planner.php
+++ progress-planner.php
@@ -7,7 +7,7 @@
  * Plugin name:       Progress Planner
  * Plugin URI:        https://progressplanner.com/
  * Description:       A plugin to help you fight procrastination and get things done.
- * Requires at least: 6.2
+ * Requires at least: 6.3
  * Requires PHP:      7.4
  * Version:           1.0.0
  * Author:            Team Emilia Projects
