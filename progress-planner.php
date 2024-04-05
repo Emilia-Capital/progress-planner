9c1444d66b6c956a104b6319a8e9ff32d10837dd
Ari Stathopoulos <aristath@gmail.com>
2024-04-05 10:07:32 +0300

    Rename `includes` folder to `classes` & move autoloader to plugin file

--- progress-planner.php
+++ progress-planner.php
@@ -20,7 +20,33 @@
 define( 'PROGRESS_PLANNER_DIR', __DIR__ );
 define( 'PROGRESS_PLANNER_URL', untrailingslashit( plugin_dir_url( __FILE__ ) ) );
 
-require_once PROGRESS_PLANNER_DIR . '/includes/autoload.php';
+/**
+ * Autoload classes.
+ */
+spl_autoload_register(
+	function ( $class_name ) {
+		$prefix = 'ProgressPlanner\\';
+
+		if ( 0 !== \strpos( $class_name, $prefix ) ) {
+			return;
+		}
+
+		$class_name = \str_replace( $prefix, '', $class_name );
+
+		$parts = \explode( '\\', $class_name );
+		$file  = PROGRESS_PLANNER_DIR . '/classes/';
+		$last  = \array_pop( $parts );
+
+		foreach ( $parts as $part ) {
+			$file .= strtolower( $part ) . '/';
+		}
+		$file .= 'class-' . \str_replace( '_', '-', \strtolower( $last ) ) . '.php';
+
+		if ( \file_exists( $file ) ) {
+			require_once $file;
+		}
+	}
+);
 
 /**
  * Get the progress planner instance.
