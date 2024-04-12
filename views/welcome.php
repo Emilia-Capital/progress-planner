b99c5f9b5e71332813c54e2623388e39c79816dd
Ari Stathopoulos <aristath@gmail.com>
2024-04-12 09:47:09 +0300

    WIP Tweaks for the welcome box

--- views/welcome.php
+++ views/welcome.php
@@ -17,5 +17,10 @@ if ( Settings::get( 'license_key' ) ) {
 ?>
 <div class="prpl-widget-wrapper prpl-welcome">
 	<h1><?php esc_html_e( 'Welcome to the Progress Planner plugin!', 'progress-planner' ); ?></h1>
-	<?php Onboard::the_form(); ?>
+	<div class="inner-grid">
+		<div class="prpl-welcome__content">
+			<p><?php esc_html_e( 'Lalala lalali lalalo.', 'progress-planner' ); ?></p>
+		</div>
+		<?php Onboard::the_form(); ?>
+	</div>
 </div>
