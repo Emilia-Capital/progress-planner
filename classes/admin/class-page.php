390cb33e7f75b88d54818950fe1f48e41e75bdca
Ari Stathopoulos <aristath@gmail.com>
2024-05-28 11:50:12 +0300

    Reorder widgets

--- classes/admin/class-page.php
+++ classes/admin/class-page.php
@@ -31,16 +31,16 @@ class Page {
 		'prpl-column-main prpl-column-main-secondary' => [
 			'prpl-column prpl-column-first'  => [
 				'\Progress_Planner\Widgets\Activity_Scores',
-				'\Progress_Planner\Widgets\ToDo',
-				'\Progress_Planner\Widgets\Personal_Record_Content',
 				'\Progress_Planner\Widgets\Plugins',
-				'\Progress_Planner\Widgets\Badge_Content',
-				'\Progress_Planner\Widgets\Badge_Streak',
+				'\Progress_Planner\Widgets\Whats_New',
+				'\Progress_Planner\Widgets\ToDo',
 			],
 			'prpl-column prpl-column-second' => [
 				'\Progress_Planner\Widgets\Latest_Badge',
+				'\Progress_Planner\Widgets\Badge_Content',
+				'\Progress_Planner\Widgets\Badge_Streak',
 				'\Progress_Planner\Widgets\Badges_Progress',
-				'\Progress_Planner\Widgets\Whats_New',
+				'\Progress_Planner\Widgets\Personal_Record_Content',
 			],
 		],
 	];
