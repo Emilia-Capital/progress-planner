7a86ca125fc7c158ac38311ebdcb60263aeec0f8
Ari Stathopoulos <aristath@gmail.com>
2024-05-31 15:07:56 +0300

    Add featured images to what's new posts

--- classes/widgets/class-whats-new.php
+++ classes/widgets/class-whats-new.php
@@ -44,6 +44,9 @@ final class Whats_New extends Widget {
 				<li>
 					<a href="<?php echo \esc_url( $blog_post['link'] ); ?>" target="_blank">
 						<h3><?php echo \esc_html( $blog_post['title']['rendered'] ); ?></h3>
+						<?php if ( isset( $blog_post['featured_media']['media_details']['sizes']['medium_large']['source_url'] ) ) : ?>
+							<img src="<?php echo \esc_url( $blog_post['featured_media']['media_details']['sizes']['medium_large']['source_url'] ); ?>" alt="">
+						<?php endif; ?>
 					</a>
 					<p>
 						<?php echo \esc_html( wp_trim_words( \wp_strip_all_tags( $blog_post['content']['rendered'] ), 55 ) ); ?>
@@ -65,7 +68,7 @@ final class Whats_New extends Widget {
 	 * @return array
 	 */
 	public function get_blog_feed() {
-		$feed = \get_site_transient( 'progress_planner_blog_feed' );
+		$feed = \get_site_transient( 'progress_planner_blog_feed_with_images' );
 		if ( false === $feed ) {
 			// Get the feed using the REST API.
 			$response = \wp_remote_get( self::REMOTE_SERVER_ROOT_URL . '/wp-json/wp/v2/posts/?per_page=2' );
@@ -73,7 +76,20 @@ final class Whats_New extends Widget {
 				return [];
 			}
 			$feed = json_decode( \wp_remote_retrieve_body( $response ), true );
-			\set_site_transient( 'progress_planner_blog_feed', $feed, 1 * DAY_IN_SECONDS );
+
+			foreach ( $feed as $key => $post ) {
+				// Get the featured media.
+				$featured_media_id = $post['featured_media'];
+				if ( $featured_media_id ) {
+					$response = \wp_remote_get( self::REMOTE_SERVER_ROOT_URL . '/wp-json/wp/v2/media/' . $featured_media_id );
+					if ( ! \is_wp_error( $response ) ) {
+						$media = json_decode( \wp_remote_retrieve_body( $response ), true );
+						$post['featured_media'] = $media;
+					}
+				}
+				$feed[ $key ] = $post;
+			}
+			\set_site_transient( 'progress_planner_blog_feed_with_images', $feed, 1 * DAY_IN_SECONDS );
 		}
 		return $feed;
 	}
