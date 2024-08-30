3ae1c1b88c1ba32dacaabe115dfe2173c9222249
Joost de Valk <joost@altha.nl>
2024-08-30 14:51:07 +0200

    Fix class

--- classes/class-playground.php
+++ classes/class-playground.php
@@ -28,7 +28,8 @@ class Playground {
 		if ( ! \get_option( 'progress_planner_license_key', false ) ) {
 			$this->generate_data();
 			\update_option( 'progress_planner_license_key', str_replace( ' ', '-', $this->create_random_string( 20 ) ) );
-			\update_option( 'progress_planner_todo', 'a:2:{i:0;a:2:{s:7:"content";s:48:"Update a post to see that the plugin tracks that";s:4:"done";b:0;}i:1;a:2:{s:7:"content";s:24:"Try out Progress Planner";s:4:"done";b:0;}}' );
+			// phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.serialize_unserialize -- This is a demo.
+			\update_option( 'progress_planner_todo', unserialize( 'a:2:{i:0;a:2:{s:7:"content";s:48:"Update a post to see that the plugin tracks that";s:4:"done";b:0;}i:1;a:2:{s:7:"content";s:24:"Try out Progress Planner";s:4:"done";b:0;}}' ) );
 		}
 		\add_action( 'admin_notices', [ $this, 'admin_notices' ] );
 	}
@@ -44,9 +45,9 @@ class Playground {
 			return;
 		}
 
-		echo '<div id="progress-planner-playground-notice" class="notice notice-success">';
-		echo '<p><strong>' . esc_html__( 'Progress Planner demo', 'progress-planner' ) . '</strong><br>';
-		\esc_html_e( 'This is a demo of the Progress Planner plugin.', 'progress-planner' );
+		echo '<div id="progress-planner-playground-notice" class="notice notice-success" style="margin-bottom:40px">';
+		echo '<p style="max-width:600px;"><strong>' . esc_html__( 'Progress Planner demo', 'progress-planner' ) . '</strong><br>';
+		\esc_html_e( 'This is a demo of Progress Planner. We\'ve prefilled this site with some content to show you what the reports in Progress Planner look like. We\'ve also added a few to-do\'s for you, you can see these here and on your dashoard.', 'progress-planner' );
 		echo '</p>';
 		echo '</div>';
 	}
@@ -70,7 +71,7 @@ class Playground {
 	 */
 	private function create_random_post() {
 		$postarr = [
-			'post_title'   => $this->create_random_string( 5 ),
+			'post_title'   => str_replace( '.', '', $this->create_random_string( 5 ) ),
 			'post_content' => $this->create_random_string( wp_rand( 200, 500 ) ),
 			'post_status'  => 'publish',
 			'post_type'    => 'post',
@@ -88,11 +89,18 @@ class Playground {
 	 */
 	private function create_random_string( $length ) {
 		$words     = [ 'the', 'and', 'have', 'that', 'for', 'you', 'with', 'say', 'this', 'they', 'but', 'his', 'from', 'not', 'she', 'as', 'what', 'their', 'can', 'who', 'get', 'would', 'her', 'all', 'make', 'about', 'know', 'will', 'one', 'time', 'there', 'year', 'think', 'when', 'which', 'them', 'some', 'people', 'take', 'out', 'into', 'just', 'see', 'him', 'your', 'come', 'could', 'now', 'than', 'like', 'other', 'how', 'then', 'its', 'our', 'two', 'more', 'these', 'want', 'way', 'look', 'first', 'also', 'new', 'because', 'day', 'use', 'man', 'find', 'here', 'thing', 'give', 'many', 'well', 'only', 'those', 'tell', 'very', 'even', 'back', 'any', 'good', 'woman', 'through', 'life', 'child', 'work', 'down', 'may', 'after', 'should', 'call', 'world', 'over', 'school', 'still', 'try', 'last', 'ask', 'need' ];
-		$word_keys = \array_rand( $words, $length );
-		$sentence  = '';
-		foreach ( $word_keys as $key ) {
-			$sentence .= $words[ $key ] . ' ';
+		$sentences = '';
+
+		// phpcs:ignore Squiz.PHP.DisallowSizeFunctionsInLoops.Found -- This works fine.
+		while ( \strlen( $sentences ) < $length ) {
+			$sentence  = '';
+			$word_keys = \array_rand( $words, \min( $length - \strlen( $sentence ), \count( $words ) ) );
+			foreach ( (array) $word_keys as $key ) {
+				$sentence .= $words[ $key ] . ' ';
+			}
+			$sentences .= \ucfirst( \trim( $sentence ) ) . '.';
 		}
-		return \ucfirst( \trim( $sentence ) ) . '.';
+
+		return $sentences;
 	}
 }
