69b489e7d5b9d6f96b1aaba242d2a495eac8c5e9
Ari Stathopoulos <aristath@gmail.com>
2024-09-03 13:42:21 +0300

    Use a list and details for suggested todos

--- classes/widgets/class-suggested-tasks.php
+++ classes/widgets/class-suggested-tasks.php
@@ -59,58 +59,63 @@ final class Suggested_Tasks extends Widget {
 			<?php esc_html_e( 'Suggested tasks', 'progress-planner' ); ?>
 		</h2>
 
-		<?php foreach ( $tasks as $priority => $priority_tasks ) : ?>
-			<?php if ( empty( $priority_tasks ) ) : ?>
-				<?php continue; ?>
-			<?php endif; ?>
+		<ul>
+			<?php foreach ( $tasks as $priority => $priority_tasks ) : ?>
+				<?php if ( empty( $priority_tasks ) ) : ?>
+					<?php continue; ?>
+				<?php endif; ?>
 
-			<?php foreach ( $priority_tasks as $task_id => $task ) : ?>
-				<?php
-				$classes   = [ 'prpl-suggested-task' ];
-				$classes[] = 'prpl-suggested-task-' . $task_id;
-				if ( \in_array( $task_id, Root_Suggested_Tasks::get_dismissed_tasks(), true ) ) {
-					$classes[] = 'prpl-suggested-task-dismissed';
-				}
-				?>
-				<div class="<?php echo esc_attr( \implode( ' ', $classes ) ); ?>">
-					<h4 class="prpl-suggested-task-title">
-						<?php echo esc_html( $task['title'] ); ?>
-					</h4>
-					<p class="prpl-suggested-task-description">
-						<?php echo esc_html( $task['description'] ); ?>
-					</p>
-					<p class="prpl-suggested-task-priority">
-						<?php
-						printf(
-							/* translators: %s: priority */
-							esc_html__( 'Priority: %s', 'progress-planner' ),
-							esc_html( $task['priority'] )
-						);
-						?>
-					</p>
-
-					<button
-						type="button"
-						class="prpl-suggested-task-button"
-						data-task-id="<?php echo esc_attr( $task_id ); ?>"
-						data-task-title="<?php echo esc_attr( $task['title'] ); ?>"
-						data-action="add-todo"
-					>
-						<?php esc_html_e( 'Add to my to-do list', 'progress-planner' ); ?>
-					</button>
-					<button
-						type="button"
-						class="prpl-suggested-task-button"
-						data-task-id="<?php echo esc_attr( $task_id ); ?>"
-						data-task-title="<?php echo esc_attr( $task['title'] ); ?>"
-						data-action="dismiss"
-					>
-						<?php esc_html_e( 'Dismiss', 'progress-planner' ); ?>
-					</button>
-				</div>
+				<?php foreach ( $priority_tasks as $task_id => $task ) : ?>
+					<?php
+					$classes   = [ 'prpl-suggested-task' ];
+					$classes[] = 'prpl-suggested-task-priority-' . $priority;
+					$classes[] = 'prpl-suggested-task-' . $task_id;
+					if ( \in_array( $task_id, Root_Suggested_Tasks::get_dismissed_tasks(), true ) ) {
+						$classes[] = 'prpl-suggested-task-dismissed';
+					}
+					?>
+					<li class="<?php echo esc_attr( \implode( ' ', $classes ) ); ?>">
+						<details>
+							<summary>
+								<?php echo esc_html( $task['title'] ); ?>
+							</summary>
+							<p class="prpl-suggested-task-description">
+								<?php echo esc_html( $task['description'] ); ?>
+							</p>
+							<p class="prpl-suggested-task-priority">
+								<?php
+								printf(
+									/* translators: %s: priority */
+									esc_html__( 'Priority: %s', 'progress-planner' ),
+									esc_html( $task['priority'] )
+								);
+								?>
+							</p>
+							<button
+								type="button"
+								class="button prpl-suggested-task-button"
+								data-task-id="<?php echo esc_attr( $task_id ); ?>"
+								data-task-title="<?php echo esc_attr( $task['title'] ); ?>"
+								data-action="add-todo"
+							>
+								<?php esc_html_e( 'Add to my to-do list', 'progress-planner' ); ?>
+							</button>
+							<button
+								type="button"
+								class="button prpl-suggested-task-button"
+								data-task-id="<?php echo esc_attr( $task_id ); ?>"
+								data-task-title="<?php echo esc_attr( $task['title'] ); ?>"
+								data-action="dismiss"
+							>
+								<?php esc_html_e( 'Dismiss', 'progress-planner' ); ?>
+							</button>
+						</details>
+					</li>
+				<?php endforeach; ?>
 			<?php endforeach; ?>
-		<?php endforeach; ?>
 
+
+		</ul>
 		<?php
 	}
 }
