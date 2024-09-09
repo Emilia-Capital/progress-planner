6920b33e8c80325635f64affd11dea4a6e612c0f
Ari Stathopoulos <aristath@gmail.com>
2024-09-09 09:19:32 +0300

    Use wp.ajax.post

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,4 +1,4 @@
-/* global progressPlannerAjaxRequest, progressPlanner */
+/* global progressPlanner */
 document
 	.getElementById( 'prpl-settings-form' )
 	.addEventListener( 'submit', function ( event ) {
@@ -7,16 +7,12 @@ document
 		const data = form.getAll( 'prpl-settings-post-types-include[]' );
 
 		// Save the options.
-		progressPlannerAjaxRequest( {
-			url: progressPlanner.ajaxUrl,
-			data: {
-				action: 'progress_planner_save_cpt_settings',
-				_ajax_nonce: progressPlanner.nonce,
-				include_post_types: data,
-			},
-			successAction: () => {
-				window.location.reload();
-			},
+		const request = wp.ajax.post( 'progress_planner_save_cpt_settings', {
+			_ajax_nonce: progressPlanner.nonce,
+			include_post_types: data,
+		} );
+		request.done( () => {
+			window.location.reload();
 		} );
 
 		document.getElementById( 'submit-include-post-types' ).disabled = true;
