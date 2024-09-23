22062d8e3c4b71e9cb13d96ca87eff1b14527d4f
Ari Stathopoulos <aristath@gmail.com>
2024-09-23 14:08:32 +0300

    Create a view file

--- classes/widgets/class-suggested-tasks.php
+++ classes/widgets/class-suggested-tasks.php
@@ -33,58 +33,8 @@ final class Suggested_Tasks extends Widget {
 		</h2>
 
 		<ul style="display:none">
-			<li id="prpl-suggested-task-template">
-				<h3>{taskTitle}</h3>
-				<p class="prpl-suggested-task-description">{taskDescription}</p>
-				<div class="actions">
-					<button
-						type="button"
-						class="button prpl-suggested-task-button"
-						data-task-id="{taskId}"
-						data-task-title="{taskTitle}"
-						data-action="add-todo"
-						title="<?php esc_attr_e( 'Add to todo list', 'progress-planner' ); ?>"
-					>
-						<span class="dashicons dashicons-list-view"></span>
-						<span class="screen-reader-text"><?php esc_html_e( 'Add to todo list', 'progress-planner' ); ?></span>
-					</button>
-					<button
-						type="button"
-						class="button prpl-suggested-task-button"
-						data-task-id="{taskId}"
-						data-task-title="{taskTitle}"
-						data-action="dismiss"
-						title="<?php esc_html_e( 'Dismiss', 'progress-planner' ); ?>"
-					>
-						<span class="dashicons dashicons-no"></span>
-						<span class="screen-reader-text"><?php esc_html_e( 'Dismiss', 'progress-planner' ); ?></span>
-					</button>
-					<button
-						type="button"
-						class="button prpl-suggested-task-button"
-						data-task-id="{taskId}"
-						data-task-title="{taskTitle}"
-						data-action="complete"
-						title="<?php esc_html_e( 'Mark as complete', 'progress-planner' ); ?>"
-					>
-						<span class="dashicons dashicons-yes"></span>
-						<span class="screen-reader-text"><?php esc_html_e( 'Mark as complete', 'progress-planner' ); ?></span>
-					</button>
-					<button
-						type="button"
-						class="button prpl-suggested-task-button"
-						data-task-id="{taskId}"
-						data-task-title="{taskTitle}"
-						data-action="snooze"
-						title="<?php esc_html_e( 'Snooze for a week', 'progress-planner' ); ?>"
-					>
-						<span class="dashicons dashicons-clock"></span>
-						<span class="screen-reader-text"><?php esc_html_e( 'Snooze for a week', 'progress-planner' ); ?></span>
-					</button>
-				</div>
-			</li>
+			<?php include PROGRESS_PLANNER_DIR . '/views/suggested-todo-item.php'; ?>
 		</ul>
-
 		<ul class="prpl-suggested-todos-list priority-high"></ul>
 		<ul class="prpl-suggested-todos-list priority-medium"></ul>
 		<ul class="prpl-suggested-todos-list priority-low"></ul>
---
+++ views/suggested-todo-item.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Template for suggested-todo item.
+ *
+ * @package Progress_Planner
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+?>
+
+<li id="prpl-suggested-task-template">
+	<h3>{taskTitle}</h3>
+	<p class="prpl-suggested-task-description">{taskDescription}</p>
+	<div class="actions">
+		<button
+			type="button"
+			class="button prpl-suggested-task-button"
+			data-task-id="{taskId}"
+			data-task-title="{taskTitle}"
+			data-action="add-todo"
+			title="<?php esc_attr_e( 'Add to todo list', 'progress-planner' ); ?>"
+		>
+			<span class="dashicons dashicons-list-view"></span>
+			<span class="screen-reader-text"><?php esc_html_e( 'Add to todo list', 'progress-planner' ); ?></span>
+		</button>
+		<button
+			type="button"
+			class="button prpl-suggested-task-button"
+			data-task-id="{taskId}"
+			data-task-title="{taskTitle}"
+			data-action="dismiss"
+			title="<?php esc_html_e( 'Dismiss', 'progress-planner' ); ?>"
+		>
+			<span class="dashicons dashicons-no"></span>
+			<span class="screen-reader-text"><?php esc_html_e( 'Dismiss', 'progress-planner' ); ?></span>
+		</button>
+		<button
+			type="button"
+			class="button prpl-suggested-task-button"
+			data-task-id="{taskId}"
+			data-task-title="{taskTitle}"
+			data-action="complete"
+			title="<?php esc_html_e( 'Mark as complete', 'progress-planner' ); ?>"
+		>
+			<span class="dashicons dashicons-yes"></span>
+			<span class="screen-reader-text"><?php esc_html_e( 'Mark as complete', 'progress-planner' ); ?></span>
+		</button>
+		<button
+			type="button"
+			class="button prpl-suggested-task-button"
+			data-task-id="{taskId}"
+			data-task-title="{taskTitle}"
+			data-action="snooze"
+			title="<?php esc_html_e( 'Snooze for a week', 'progress-planner' ); ?>"
+		>
+			<span class="dashicons dashicons-clock"></span>
+			<span class="screen-reader-text"><?php esc_html_e( 'Snooze for a week', 'progress-planner' ); ?></span>
+		</button>
+	</div>
+</li>
