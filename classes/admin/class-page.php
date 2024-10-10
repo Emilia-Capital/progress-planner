dde4820cfc282be6ea4cbb1b0a387f8185db6413
Ari Stathopoulos <aristath@gmail.com>
2024-10-10 08:53:19 +0300

    Switch layout to 3 columns

--- classes/admin/class-page.php
+++ classes/admin/class-page.php
@@ -18,33 +18,25 @@ class Page {
 	 * The columns and widgets to display on the admin page.
 	 */
 	const COLUMNS = [
-		'prpl-column-main prpl-column-main-primary'   => [
-			'prpl-column prpl-column-first' => [
-				'prpl-column prpl-column-two-col col-1' => [
-					'\Progress_Planner\Widgets\Website_Activity_Score',
-					'\Progress_Planner\Widgets\Suggested_Tasks_Score',
-				],
-				'prpl-column prpl-column-two-col col-2' => [
-					'\Progress_Planner\Widgets\Published_Content_Density',
-					'\Progress_Planner\Widgets\Published_Words',
-				],
-				'\Progress_Planner\Widgets\Published_Content',
-				'\Progress_Planner\Widgets\Whats_New',
-			],
+		[
+			'\Progress_Planner\Widgets\Website_Activity_Score',
+			'\Progress_Planner\Widgets\Suggested_Tasks_Score',
+			'\Progress_Planner\Widgets\Published_Content_Density',
+			'\Progress_Planner\Widgets\Published_Words',
+			'\Progress_Planner\Widgets\Published_Content',
+			'\Progress_Planner\Widgets\Whats_New',
 		],
-		'prpl-column-main prpl-column-main-secondary' => [
-			'prpl-column prpl-column-first'  => [
-				'\Progress_Planner\Widgets\Suggested_Tasks',
-				'\Progress_Planner\Widgets\Activity_Scores',
-				'\Progress_Planner\Widgets\Badges_Progress',
-				'\Progress_Planner\Widgets\Personal_Record_Content',
-			],
-			'prpl-column prpl-column-second' => [
-				'\Progress_Planner\Widgets\ToDo',
-				'\Progress_Planner\Widgets\Latest_Badge',
-				'\Progress_Planner\Widgets\Badge_Content',
-				'\Progress_Planner\Widgets\Badge_Streak',
-			],
+		[
+			'\Progress_Planner\Widgets\Suggested_Tasks',
+			'\Progress_Planner\Widgets\Activity_Scores',
+			'\Progress_Planner\Widgets\Badges_Progress',
+			'\Progress_Planner\Widgets\Personal_Record_Content',
+		],
+		[
+			'\Progress_Planner\Widgets\ToDo',
+			'\Progress_Planner\Widgets\Latest_Badge',
+			'\Progress_Planner\Widgets\Badge_Content',
+			'\Progress_Planner\Widgets\Badge_Streak',
 		],
 	];
 
@@ -97,42 +89,17 @@ class Page {
 			<?php \do_action( 'progress_planner_admin_after_header' ); ?>
 
 			<div class="prpl-widgets-container">
-				<?php
-				$columns = \apply_filters( 'progress_planner_admin_columns_widgets', self::COLUMNS );
-
-				foreach ( $columns as $key => $value ) {
-					$this->render_column( $key, $value );
-				}
-				?>
+				<?php $columns = \apply_filters( 'progress_planner_admin_columns_widgets', self::COLUMNS ); ?>
+				<?php foreach ( $columns as $column ) : ?>
+					<div class="prpl-column">
+						<?php foreach ( $column as $class_name ) : ?>
+							<?php new $class_name(); ?>
+						<?php endforeach; ?>
+					</div>
+				<?php endforeach; ?>
 			</div>
 		</div>
 		<?php
-	}
-
-	/**
-	 * Render a column.
-	 *
-	 * @param string       $key   The column key.
-	 * @param string|array $value The widget(s) to render.
-	 *
-	 * @return void
-	 */
-	private function render_column( $key, $value ) {
-		echo '<div class="' . \esc_attr( $key ) . '">';
-
-		if ( \is_array( $value ) ) {
-			foreach ( $value as $sub_key => $sub_value ) {
-				if ( \is_string( $sub_value ) && \str_starts_with( $sub_value, '\\' ) ) {
-					new $sub_value();
-					continue;
-				}
-				$this->render_column( $sub_key, $sub_value );
-			}
-		} elseif ( \str_starts_with( $value, '\\' ) ) {
-			new $value();
-		}
-
-		echo '</div>';
 	}
 
 	/**
