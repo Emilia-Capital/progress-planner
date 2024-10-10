d8a1fbab51566c82324790307789279f78e2e2a0
Filip Ilic <ilic.filip@gmail.com>
2024-10-10 09:51:14 +0200

    change how blog feed is stored in a transient

--- classes/widgets/class-whats-new.php
+++ classes/widgets/class-whats-new.php
@@ -66,31 +66,58 @@ final class Whats_New extends Widget {
 	 * @return array
 	 */
 	public function get_blog_feed() {
-		$feed = \get_site_transient( 'progress_planner_blog_feed_with_images' );
-		if ( false === $feed ) {
+		$feed_data = \get_site_transient( 'progress_planner_blog_feed_with_images' );
+
+		// Migrate old feed to new format.
+		if ( is_array( $feed_data ) && ! isset( $feed_data['expires'] ) && ! isset( $feed_data['feed'] ) ) {
+			$feed_data = [
+				'feed'    => $feed_data,
+				'expires' => get_option( '_site_transient_timeout_progress_planner_blog_feed_with_images', 0 ),
+			];
+		}
+
+		// Transient not set.
+		if ( false === $feed_data ) {
+			$feed_data = [
+				'feed'    => [],
+				'expires' => 0,
+			];
+		}
+
+		// Transient expired, fetch new feed.
+		if ( $feed_data['expires'] < time() ) {
 			// Get the feed using the REST API.
 			$response = \wp_remote_get( self::REMOTE_SERVER_ROOT_URL . '/wp-json/wp/v2/posts/?per_page=2' );
-			if ( \is_wp_error( $response ) ) {
-				return [];
-			}
-			$feed = json_decode( \wp_remote_retrieve_body( $response ), true );
 
-			foreach ( $feed as $key => $post ) {
-				// Get the featured media.
-				$featured_media_id = $post['featured_media'];
-				if ( $featured_media_id ) {
-					$response = \wp_remote_get( self::REMOTE_SERVER_ROOT_URL . '/wp-json/wp/v2/media/' . $featured_media_id );
-					if ( ! \is_wp_error( $response ) ) {
-						$media = json_decode( \wp_remote_retrieve_body( $response ), true );
+			if ( 200 !== wp_remote_retrieve_response_code( $response ) ) {
+				// If we cant fetch the feed, we will try again later.
+				$feed_data['expires'] = time() + 5 * MINUTE_IN_SECONDS;
+			} else {
+				$feed = json_decode( \wp_remote_retrieve_body( $response ), true );
 
-						$post['featured_media'] = $media;
+				foreach ( $feed as $key => $post ) {
+					// Get the featured media.
+					$featured_media_id = $post['featured_media'];
+					if ( $featured_media_id ) {
+						$response = \wp_remote_get( self::REMOTE_SERVER_ROOT_URL . '/wp-json/wp/v2/media/' . $featured_media_id );
+						if ( ! \is_wp_error( $response ) ) {
+							$media = json_decode( \wp_remote_retrieve_body( $response ), true );
+
+							$post['featured_media'] = $media;
+						}
 					}
+					$feed[ $key ] = $post;
 				}
-				$feed[ $key ] = $post;
+
+				$feed_data['feed']    = $feed;
+				$feed_data['expires'] = time() + 1 * DAY_IN_SECONDS;
 			}
-			\set_site_transient( 'progress_planner_blog_feed_with_images', $feed, 1 * DAY_IN_SECONDS );
+
+			// Transient uses 'expires' key to determine if it's expired.
+			\set_site_transient( 'progress_planner_blog_feed_with_images', $feed_data, 0 );
 		}
-		return $feed;
+
+		return $feed_data['feed'];
 	}
 }
 
