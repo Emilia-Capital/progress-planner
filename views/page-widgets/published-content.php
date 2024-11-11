e2fb8420a2be94462b5abf98ba98e3684d1879de
Ari Stathopoulos <aristath@gmail.com>
2024-11-11 12:33:22 +0200

    Simplify widget some more

--- views/page-widgets/published-content.php
+++ views/page-widgets/published-content.php
@@ -79,41 +79,9 @@ $prpl_sum_weekly = array_sum( $prpl_stats['weekly'] );
 </div>
 <p>
 	<?php
-	if ( 0 === $prpl_widget->get_weekly_activities_density() ) {
-		printf(
-			/* translators: %s: All-time average number. */
-			\esc_html__( 'Your overall content density average is %1$s.', 'progress-planner' ),
-			\esc_html( \number_format_i18n( $prpl_widget->get_all_activities_density() ) )
-		);
+	if ( 0 === $prpl_widget->get_weekly_words() ) {
+		\esc_html_e( 'You didn\'t write last week. Let\'s get started!', 'progress-planner' );
 	} else {
-		printf(
-			/* translators: %1$s: number of words/post published this week. %2$s: All-time average number. */
-			\esc_html__( 'Your content has an average density of %1$s words per post in the last 7 days. Your overall content density average is %2$s.', 'progress-planner' ),
-			\esc_html( \number_format_i18n( $prpl_widget->get_weekly_activities_density() ) ),
-			\esc_html( \number_format_i18n( $prpl_widget->get_all_activities_density() ) )
-		);
-	}
-	?>
-</p>
-<div class="prpl-graph-wrapper">
-	<?php \progress_planner()->get_chart()->the_chart( $prpl_widget->get_chart_args_content_density() ); ?>
-</div>
-
-<hr>
-
-<div class="prpl-counter-big-wrapper">
-	<span class="counter-big-number">
-		<?php echo \esc_html( \number_format_i18n( (int) $prpl_widget->get_weekly_words() ) ); ?>
-	</span>
-	<span class="counter-big-text">
-		<?php echo \esc_html_e( 'words', 'progress-planner' ); ?>
-	</span>
-</div>
-<p>
-	<?php if ( 0 === $prpl_widget->get_weekly_words() ) : ?>
-		<?php \esc_html_e( 'You didn\'t write last week. Let\'s get started!', 'progress-planner' ); ?>
-	<?php else : ?>
-		<?php
 		printf(
 			\esc_html(
 				/* translators: %1$s: number of posts published this week. %2$s: Total number of posts. */
@@ -124,8 +92,30 @@ $prpl_sum_weekly = array_sum( $prpl_stats['weekly'] );
 					'progress-planner'
 				)
 			),
-			\esc_html( \number_format_i18n( $prpl_widget->get_weekly_words() ) ),
+			'<strong>' . \esc_html( \number_format_i18n( $prpl_widget->get_weekly_words() ) ) . '</strong>',
 		);
-		?>
-	<?php endif; ?>
+	}
+	?>
 </p>
+
+<p>
+	<?php
+	if ( 0 === $prpl_widget->get_weekly_activities_density() ) {
+		printf(
+			/* translators: %s: All-time average number. */
+			\esc_html__( 'Your overall content density average is %1$s.', 'progress-planner' ),
+			'<strong>' . \esc_html( \number_format_i18n( $prpl_widget->get_all_activities_density() ) ) . '</strong>'
+		);
+	} else {
+		printf(
+			/* translators: %1$s: number of words/post published this week. %2$s: All-time average number. */
+			\esc_html__( 'Your content has an average density of %1$s words per post in the last 7 days. Your overall content density average is %2$s.', 'progress-planner' ),
+			'<strong>' . \esc_html( \number_format_i18n( $prpl_widget->get_weekly_activities_density() ) ) . '</strong>',
+			'<strong>' . \esc_html( \number_format_i18n( $prpl_widget->get_all_activities_density() ) ) . '</strong>'
+		);
+	}
+	?>
+</p>
+<div class="prpl-graph-wrapper">
+	<?php \progress_planner()->get_chart()->the_chart( $prpl_widget->get_chart_args_content_density() ); ?>
+</div>
