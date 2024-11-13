67d1c7f481f7cd1fb3b77d4518f11d3cb53d0e70
Ari Stathopoulos <aristath@gmail.com>
2024-11-13 10:47:43 +0200

    cleanup

--- assets/js/web-components/prpl-gauge.js
+++ assets/js/web-components/prpl-gauge.js
@@ -17,36 +17,9 @@ customElements.define(
 			const color = this.getAttribute( 'color' ) || 'var(--prpl-color-accent-orange)';
 			const start = this.getAttribute( 'start' ) || '-135deg';
 			const cutout = this.getAttribute( 'cutout' ) || '57%';
-			const hasImage = ( this.querySelector( 'img' ) || this.querySelector( 'svg' ) ) ? true : false;
 			const contentFontSize = this.getAttribute( 'contentFontSize' ) || 'var(--prpl-font-size-6xl)';
-			const containerStyles = `
-				padding:var(--prpl-padding);
-				background:${ background };
-				border-radius:var(--prpl-border-radius);
-				aspect-ratio: 2 / 1;
-				overflow: hidden;
-				position: relative;
-				margin-bottom: var(--prpl-padding);
-			`;
-			const gaugeStyles = `
-				width: 100%;
-				aspect-ratio: 1 / 1;
-				border-radius: 100%;
-				position: relative;
-				background: radial-gradient(${background} 0 ${cutout}, transparent ${cutout} 100%), conic-gradient(from ${start}, ${color} calc(${maxDeg} * ${value}), var(--prpl-color-gray-1) calc(${maxDeg} * ${value}) ${maxDeg}, transparent ${maxDeg});
-				text-align: center;
-			`;
-			const numberIndicatorStyles = `
-				font-size: var(--prpl-font-size-small);
-				position: absolute;
-				top: 50%;
-				color: var(--prpl-color-gray-5);
-				width: 10%;
-				text-align: center;
-			`;
-
 			let content = this.innerHTML;
-			if ( hasImage ) {
+			if ( this.querySelector( 'img' ) || this.querySelector( 'svg' ) ) {
 				content = `
 					<span style="top: -1em; display: block; padding-top: 25%; text-align: center; position: absolute;width: 100%;">
 						<span style="display:inline-block;width: 50%;">
@@ -63,11 +36,11 @@ customElements.define(
 			}
 
 			this.innerHTML = `
-			<div style="${ containerStyles }">
-				<div style="${ gaugeStyles }">
-					<span style="${ numberIndicatorStyles }left:0;">0</span>
+			<div style="padding: var(--prpl-padding); background: ${ background }; border-radius:var(--prpl-border-radius); aspect-ratio: 2 / 1; overflow: hidden; position: relative;margin-bottom: var(--prpl-padding);">
+				<div style="width: 100%; aspect-ratio: 1 / 1; border-radius: 100%; position: relative; background: radial-gradient(${ background } 0 ${ cutout }, transparent ${ cutout } 100%), conic-gradient(from ${ start }, ${ color } calc(${ maxDeg } * ${ value }), var(--prpl-color-gray-1) calc(${ maxDeg } * ${ value }) ${ maxDeg }, transparent ${ maxDeg }); text-align: center;">
+					<span style="font-size: var(--prpl-font-size-small); position: absolute; top: 50%; color: var(--prpl-color-gray-5); width: 10%; text-align: center; left:0;">0</span>
 					${ content }
-					<span style="${ numberIndicatorStyles }right:0;">${ max }</span>
+					<span style="font-size: var(--prpl-font-size-small); position: absolute; top: 50%; color: var(--prpl-color-gray-5); width: 10%; text-align: center; right:0;">${ max }</span>
 				</div>
 			</div>
 			`;
