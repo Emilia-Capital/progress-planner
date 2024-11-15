0d06adabd40ffc3aec41ad69ee7be7fbfe1dd97b
Filip Ilic <ilic.filip@gmail.com>
2024-11-15 12:54:35 +0100

    monthly badges in popover are always visible & greyed out
    popover monthly badges for 2025 are not scrollable inside popover

--- views/page-widgets/parts/monthly-badge-2024.php
+++ views/page-widgets/parts/monthly-badge-2024.php
@@ -13,23 +13,31 @@ $prpl_badge           = \progress_planner()->get_badges()->get_badge( 'monthly-2
 $prpl_badge_progress  = $prpl_badge->get_progress();
 $prpl_badge_completed = 100 === (int) $prpl_badge_progress['progress'];
 
-if ( ! $prpl_badge_completed ) {
+$prpl_location  = '';
+$prpl_css_class = '';
+
+if ( isset( $args['css_class'] ) ) {
+	$prpl_css_class = \esc_attr( $args['css_class'] );
+}
+
+$prpl_location = false !== strpos( $prpl_css_class, 'in-popover' ) ? 'popover' : 'suggested-tasks';
+
+if ( ! $prpl_badge_completed && 'popover' !== $prpl_location ) {
 	return;
 }
 
 $prpl_title_tag = isset( $args['title_tag'] ) ? \esc_attr( $args['title_tag'] ) : 'h3';
 ?>
 
-<div class="<?php echo \esc_attr( isset( $args['css_class'] ) ? \esc_attr( $args['css_class'] ) : '' ); ?>">
+<div class="<?php echo \esc_attr( $prpl_css_class ); ?>">
 	<<?php echo \esc_html( $prpl_title_tag ); ?> class="prpl-widget-title">
 		<?php \esc_html_e( 'Your monthly badge 2024', 'progress-planner' ); ?>
 	</<?php echo \esc_html( $prpl_title_tag ); ?>>
 	<div class="prpl-ravi-reward-container">
-		<span class="prpl-ravi-reward-graphic">
-			<img
-				src="<?php echo \esc_url( \progress_planner()->get_admin__page()->get_widget( 'latest-badge' )->endpoint . $prpl_badge->get_id() ); ?>"
-				alt="<?php echo \esc_attr( $prpl_badge->get_name() ); ?>"
-			/>
-		</span>
+		<img
+			src="<?php echo \esc_url( \progress_planner()->get_admin__page()->get_widget( 'latest-badge' )->endpoint . $prpl_badge->get_id() ); ?>"
+			alt="<?php echo \esc_attr( $prpl_badge->get_name() ); ?>"
+			class="<?php echo $prpl_badge_completed ? 'complete' : 'incomplete'; ?>"
+		/>
 	</div>
 </div>
