c07e472ff5fa5b139f13cd3d0cd6909373959cf1
Ari Stathopoulos <aristath@gmail.com>
2024-11-21 14:18:33 +0200

    Use `<svg>` for line graphs & <table>` for bar graphs

--- classes/class-chart.php
+++ classes/class-chart.php
@@ -50,7 +50,7 @@ class Chart {
 		$chart_params = \wp_parse_args( $args['chart_params'], static::DEFAULT_CHART_PARAMS );
 
 		// Render the chart.
-		$this->render_chart_js(
+		$this->render_chart(
 			md5( \wp_json_encode( $args ) ) . \wp_rand( 0, 1000 ),
 			$chart_params['type'],
 			$this->get_chart_data( $args ),
@@ -265,7 +265,7 @@ class Chart {
 	}
 
 	/**
-	 * Render the chart using Chart.js.
+	 * Render the charts.
 	 *
 	 * @param string $id      The ID of the chart.
 	 * @param string $type    The type of chart.
@@ -274,35 +274,203 @@ class Chart {
 	 *
 	 * @return void
 	 */
-	public function render_chart_js( $id, $type, $data, $options = [] ) {
-		$id = 'progress-planner-chart-' . $id;
+	public function render_chart( $id, $type, $data, $options = [] ) { // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter
+		switch ( $type ) {
+			case 'bar':
+				$this->render_bar_chart_native( $data );
+				break;
+
+			default:
+				$this->render_line_chart_svg( $data );
+				break;
+		}
+	}
+
+	/**
+	 * Render the contents of a bar chart.
+	 *
+	 * @param array $data The data for the chart.
+	 *
+	 * @return void
+	 */
+	public function render_bar_chart_native( $data ) {
+		?>
+		<table style="width:100%;border-collapse:collapse;">
+			<tbody>
+				<tr>
+					<?php foreach ( $data['datasets'][0]['data'] as $i => $score ) : ?>
+						<td valign="bottom" style="height:200px;">
+							<div style="
+								height: <?php echo esc_attr( $score ); ?>%;
+								width: 90%;
+								background: <?php echo esc_attr( $data['datasets'][0]['backgroundColor'][ $i ] ); ?>
+							"></div>
+						</td>
+					<?php endforeach; ?>
+				</tr>
+			</tbody>
+			<tfoot>
+				<tr>
+					<?php foreach ( $data['labels'] as $label ) : ?>
+						<td style="text-align: center; font-size:0.75em;"><?php echo esc_html( $label ); ?></td>
+					<?php endforeach; ?>
+				</tr>
+			</tfoot>
+		</table>
+		<?php
+	}
+
+	/**
+	 * Render the SVG contents of a line chart.
+	 *
+	 * @param array $data The data for the chart.
+	 * @param int   $max  The maximum value for the chart.
+	 *
+	 * @return void
+	 */
+	public function render_line_chart_svg( $data, $max = 100 ) {
+		$aspect_ratio = 1.7;
+		$height       = 300;
+		$axis_offset  = 16;
+		$width        = $height * $aspect_ratio;
+
+		echo '<svg viewBox="0 0 ' . (int) ( $height * $aspect_ratio + $axis_offset * 2 ) . ' ' . (int) ( $height + $axis_offset * 2 ) . '">';
+
+		// Determine the maximum value for the chart.
+		$max = max(
+			array_map(
+				function ( $dataset ) {
+					return max( $dataset['data'] );
+				},
+				$data['datasets']
+			)
+		);
+
+		// Calculate the Y coordinate for a given value.
+		$calc_y_coordinate = function ( $value ) use ( $max, $axis_offset, $height ) {
+			// Calculate scaled coordinates because of the offset and the aspect ratio.
+			$multiplier   = ( $height - $axis_offset * 2 ) / $height;
+			$y_coordinate = ( $max - $value * $multiplier ) * ( $height / $max ) + $axis_offset;
+			// Offset the Y coordinate to account for the axis offset.
+			$y_coordinate -= $axis_offset * 2;
+			// Remove the last 1px of the line to account for the stroke width.
+			return $y_coordinate - 1;
+		};
+
+		// Calculate the Y axis labels.
+		// Take the maximum value and divide it by 4 to get the step.
+		$y_labels_step = $max / 4;
+
+		// Calculate the Y axis labels.
+		$y_labels = [];
+		if ( 100 === $max ) {
+			for ( $i = 0; $i <= 4; $i++ ) {
+				$y_labels[] = $y_labels_step * $i;
+			}
+		} else {
+			// Round the values to the nearest 10.
+			for ( $i = 0; $i <= 4; $i++ ) {
+				$y_labels[] = min( $max, round( $y_labels_step * $i, -1 ) );
+			}
+		}
+
+		// Calculate the distance between the points in the X axis.
+		// Calculate the distance between the points in the X axis.
+		$x_distance_between_points = round(
+			( $width - ( 2 * $axis_offset ) ) / ( count( $data['datasets'][0]['data'] ) - 1 ),
+			0,
+			PHP_ROUND_HALF_DOWN
+		);
 		?>
 
-		<div class="prpl-chart-container">
-			<canvas id="<?php echo \sanitize_key( $id ); ?>"></canvas>
-		</div>
-		<script>
-			var chartOptions = <?php echo \wp_json_encode( $options ); ?>,
-				previousXAxisLabel = '';
+		<!-- X-axis line. -->
+		<g><line
+			x1="<?php echo (int) $axis_offset * 2; ?>"
+			x2="<?php echo (int) $aspect_ratio * $height; ?>"
+			y1="<?php echo (int) $height - $axis_offset; ?>"
+			y2="<?php echo (int) $height - $axis_offset; ?>"
+			stroke="var(--prpl-color-gray-2)"
+			stroke-width="1"
+			/>
+		</g>
 
-			chartOptions.scales = chartOptions.scales || {};
-			chartOptions.scales.xAxis = chartOptions.scales.xAxis || {};
-			chartOptions.scales.xAxis.ticks = {
-				callback: function( val, index ) {
-					var label = this.getLabelForValue( val );
-					if ( label !== previousXAxisLabel ) {
-						previousXAxisLabel = label;
-						return label;
-					}
-				},
-			};
+		<!-- Y-axis line. -->
+		<g><line
+			x1="<?php echo (int) $axis_offset * 2; ?>"
+			x2="<?php echo (int) $axis_offset * 2; ?>"
+			y1="<?php echo (int) $axis_offset; ?>"
+			y2="<?php echo (int) $height - $axis_offset; ?>"
+			stroke="var(--prpl-color-gray-2)"
+			stroke-width="1"
+			/>
+		</g>
 
-			var chart = new Chart( document.getElementById( '<?php echo \sanitize_key( $id ); ?>' ), {
-				type: '<?php echo \esc_js( $type ); ?>',
-				data: <?php echo \wp_json_encode( $data ); ?>,
-				options: chartOptions,
-			} );
-		</script>
+		<!-- X-axis labels. -->
+		<g>
+			<?php
+			$label_x_coordinate = 0;
+			$labels_x_count     = count( $data['labels'] );
+			$labels_x_divider   = intval( round( $labels_x_count / 6, 0, PHP_ROUND_HALF_UP ) );
+			$i                  = 0;
+			?>
+			<?php foreach ( $data['labels'] as $label ) : ?>
+				<?php $label_x_coordinate = $x_distance_between_points * $i + $axis_offset; ?>
+				<?php ++$i; ?>
+				<?php
+				// Only allow up to 6 labels to prevent overlapping.
+				// If there are more than 6 labels, find the alternate labels.
+				if ( 6 < $labels_x_count && 1 !== $i && ( $i - 1 ) % $labels_x_divider !== 0 ) {
+					continue;
+				}
+				?>
+				<text
+					class="x-axis-label"
+					x="<?php echo (int) $label_x_coordinate; ?>"
+					y="<?php echo (int) $height + $axis_offset; ?>"
+				>
+					<?php echo \esc_html( (string) $label ); ?>
+				</text>
+			<?php endforeach; ?>
+		</g>
+
+		<!-- Y-axis labels. -->
+		<g>
+			<?php $i = 0; ?>
+			<?php foreach ( $y_labels as $y_label ) : ?>
+				<text
+					class="y-axis-label"
+					x="0"
+					y="<?php echo (int) $calc_y_coordinate( $y_label ) + $axis_offset / 2; ?>"
+				>
+					<?php echo \esc_html( (string) $y_label ); ?>
+				</text>
+				<?php ++$i; ?>
+			<?php endforeach; ?>
+		</g>
+
+		<!-- Line chart. -->
+		<?php foreach ( $data['datasets'] as $dataset ) : ?>
+			<?php
+			$points       = [];
+			$x_coordinate = $axis_offset * 2;
+			foreach ( $dataset['data'] as $point ) {
+				$points[]      = [ $x_coordinate, $calc_y_coordinate( $point ) ];
+				$x_coordinate += $x_distance_between_points;
+			}
+			?>
+
+			<g><polyline
+				fill="none"
+				stroke="<?php echo \esc_attr( $dataset['borderColor'][0] ); ?>"
+				stroke-width="2"
+				points="
+					<?php foreach ( $points as $point ) : ?>
+						<?php echo \esc_attr( implode( ',', $point ) ); ?>
+					<?php endforeach; ?>
+				"
+			/></g>
+		<?php endforeach; ?>
 		<?php
+		echo '</svg>';
 	}
 }
