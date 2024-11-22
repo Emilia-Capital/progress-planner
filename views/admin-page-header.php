b035cc02ac61494afdececa54ff89d897d472a79
Filip Ilic <ilic.filip@gmail.com>
2024-11-22 14:54:10 +0100

    style email icon

--- views/admin-page-header.php
+++ views/admin-page-header.php
@@ -24,16 +24,14 @@ $progress_planner_active_frequency = isset( $_GET['frequency'] ) ? \sanitize_tex
 
 	<div class="prpl-header-right">
 		<button class="prpl-info-icon" id="prpl-start-tour-icon-button" onclick="prplStartTour()">
-			<span class="prpl-tour-icon">
-				<?php \progress_planner()->the_asset( 'images/tour_icon.svg' ); ?>
-			</span>
+			<?php \progress_planner()->the_asset( 'images/tour_icon.svg' ); ?>
 			<span class="screen-reader-text"><?php \esc_html_e( 'Start tour', 'progress-planner' ); ?>
 		</button>
 		<?php
 		// Render the settings button.
 		\progress_planner()->get_popover()->the_popover( 'settings' )->render_button(
 			'',
-			'<span class="prpl-settings-icon">' . \progress_planner()->get_asset( 'images/settings_icon.svg' ) . '</span><span class="screen-reader-text">' . \esc_html__( 'Settings', 'progress-planner' ) . '</span>'
+			\progress_planner()->get_asset( 'images/settings_icon.svg' ) . '<span class="screen-reader-text">' . \esc_html__( 'Settings', 'progress-planner' ) . '</span>'
 		);
 		// Render the settings popover.
 		\progress_planner()->get_popover()->the_popover( 'settings' )->render();
