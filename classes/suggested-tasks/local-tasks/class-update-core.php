04131bc3b171f1a17e03c0a4896b7406d26807c3
Filip Ilic <ilic.filip@gmail.com>
2024-12-06 13:28:19 +0100

    fix \wp_get_update_data returning incorrect count for core

--- classes/suggested-tasks/local-tasks/class-update-core.php
+++ classes/suggested-tasks/local-tasks/class-update-core.php
@@ -20,6 +20,12 @@ class Update_Core implements \Progress_Planner\Suggested_Tasks\Local_Tasks_Inter
 	 * @return bool|string
 	 */
 	public function evaluate_task( $task_id ) {
+
+		// Without this \wp_get_update_data() might not return correct data for the core updates (depending on the timing).
+		if ( ! function_exists( 'get_core_updates' ) ) {
+			require_once ABSPATH . 'wp-admin/includes/update.php'; // @phpstan-ignore requireOnce.fileNotFound
+		}
+
 		if ( 0 === strpos( $task_id, 'update-core' ) && 0 === \wp_get_update_data()['counts']['total'] ) {
 			return $task_id;
 		}
@@ -41,6 +47,12 @@ class Update_Core implements \Progress_Planner\Suggested_Tasks\Local_Tasks_Inter
 	 * @return array
 	 */
 	public function get_tasks_to_update_core() {
+
+		// Without this \wp_get_update_data() might not return correct data for the core updates (depending on the timing).
+		if ( ! function_exists( 'get_core_updates' ) ) {
+			require_once ABSPATH . 'wp-admin/includes/update.php'; // @phpstan-ignore requireOnce.fileNotFound
+		}
+
 		// If all updates are performed, do not add the task.
 		if ( 0 === \wp_get_update_data()['counts']['total'] ) {
 			return [];
