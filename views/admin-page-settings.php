de959fe7d3d1ae9c8da8cf82a9438ce5fa6bfa6d
Ari Stathopoulos <aristath@gmail.com>
2024-12-10 12:50:31 +0200

    Add free-license field

--- views/admin-page-settings.php
+++ views/admin-page-settings.php
@@ -50,6 +50,34 @@ if ( ! defined( 'ABSPATH' ) ) {
 			</div>
 		</div>
 
+		<div class="prpl-column">
+			<div class="prpl-widget-wrapper">
+				<h2 class="prpl-settings-section-license">
+					<span>
+						<?php esc_html_e( 'Your licenses', 'progress-planner' ); ?>
+					</span>
+				</h2>
+				<div>
+					<label>
+						<?php
+						$prpl_free_license_key = get_option( 'progress_planner_license_key', '' );
+						if ( 'no-license' === $prpl_free_license_key ) {
+							$prpl_free_license_key = '';
+						}
+						?>
+						<input type="text" value="<?php echo esc_attr( $prpl_free_license_key ); ?>" disabled />
+						<?php if ( '' === $prpl_free_license_key ) : ?>
+							<a href="<?php echo \esc_url( \admin_url( 'admin.php?page=progress-planner#prpl-popover-subscribe-form-trigger' ) ); ?>">
+								<?php esc_html_e( 'Please register to weekly emails to get a free license.', 'progress-planner' ); ?>
+							</a>
+						<?php else : ?>
+							<?php \esc_html_e( 'You automatically received a free license when you registered to weekly emails.', 'progress-planner' ); ?>
+						<?php endif; ?>
+					</label>
+				</div>
+			</div>
+		</div>
+
 		<?php wp_nonce_field( 'prpl-settings' ); ?>
 
 		<button
