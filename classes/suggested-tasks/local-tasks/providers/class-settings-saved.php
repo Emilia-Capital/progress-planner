39a8eb837b05f2c5d27efbe3555e30c53f365a6d
Filip Ilic <ilic.filip@gmail.com>
2024-12-13 10:23:45 +0100

    add filling settings page task

---
+++ classes/suggested-tasks/local-tasks/providers/class-settings-saved.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * Add tasks for settings saved.
+ *
+ * @package Progress_Planner
+ */
+
+namespace Progress_Planner\Suggested_Tasks\Local_Tasks\Providers;
+
+/**
+ * Add tasks for settings saved.
+ */
+class Settings_Saved implements \Progress_Planner\Suggested_Tasks\Local_Tasks\Providers\Local_Tasks_Interface {
+
+	/**
+	 * The provider ID.
+	 *
+	 * @var string
+	 */
+	const TYPE = 'settings-saved';
+
+	/**
+	 * Get the provider ID.
+	 *
+	 * @return string
+	 */
+	public function get_provider_type() {
+		return self::TYPE;
+	}
+
+	/**
+	 * Evaluate a task.
+	 *
+	 * @param string $task_id The task ID.
+	 *
+	 * @return bool|string
+	 */
+	public function evaluate_task( $task_id ) {
+		if ( 0 === strpos( $task_id, self::TYPE ) && false !== \get_option( 'progress_planner_pro_license_key', false ) ) {
+			return $task_id;
+		}
+		return false;
+	}
+
+	/**
+	 * Get an array of tasks to inject.
+	 *
+	 * @return array
+	 */
+	public function get_tasks_to_inject() {
+		if ( true === $this->is_task_type_snoozed() ) {
+			return [];
+		}
+
+		$prpl_pro_license_key = \get_option( 'progress_planner_pro_license_key', false );
+
+		if ( false !== $prpl_pro_license_key ) {
+			return [];
+		}
+
+		$task_id = self::TYPE . '-' . \gmdate( 'YW' );
+
+		// If the task with this id is completed, don't add a task.
+		if ( true === \progress_planner()->get_suggested_tasks()->check_task_condition(
+			[
+				'type'    => 'completed',
+				'task_id' => $task_id,
+			]
+		) ) {
+			return [];
+		}
+
+		return [
+			$this->get_task_details( self::TYPE . '-' . \gmdate( 'YW' ) ),
+		];
+	}
+
+	/**
+	 * Get the task details.
+	 *
+	 * @param string $task_id The task ID.
+	 *
+	 * @return array
+	 */
+	public function get_task_details( $task_id ) {
+
+		return [
+			'task_id'     => $task_id,
+			'title'       => \esc_html__( 'Fill settings page', 'progress-planner' ),
+			'parent'      => 0,
+			'priority'    => 'high',
+			'type'        => 'maintenance',
+			'points'      => 1,
+			'url'         => \esc_url( \admin_url( 'admin.php?page=progress-planner-settings' ) ),
+			'description' => '<p>' . \esc_html__( 'Head over to the settings page and fill in the required information.', 'progress-planner' ) . '</p>',
+		];
+	}
+
+	/**
+	 * Get the data from a task-ID.
+	 *
+	 * @param string $task_id The task ID.
+	 *
+	 * @return array The data.
+	 */
+	public function get_data_from_task_id( $task_id ) {
+		$data = [
+			'type' => self::TYPE,
+			'id'   => $task_id,
+		];
+
+		return $data;
+	}
+
+	/**
+	 * Check if a task type is snoozed.
+	 *
+	 * @return bool
+	 */
+	public function is_task_type_snoozed() {
+		$snoozed = \progress_planner()->get_suggested_tasks()->get_snoozed_tasks();
+		if ( ! \is_array( $snoozed ) || empty( $snoozed ) ) {
+			return false;
+		}
+
+		foreach ( $snoozed as $task ) {
+			if ( self::TYPE === $task['id'] ) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
