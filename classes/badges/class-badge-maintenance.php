f85f8d588734dc0ae9c3b8f628b76d9ec1db3982
Ari Stathopoulos <aristath@gmail.com>
2025-01-15 14:40:52 +0200

    Add a $background prop in badges classes

--- classes/badges/class-badge-maintenance.php
+++ classes/badges/class-badge-maintenance.php
@@ -16,6 +16,13 @@ use Progress_Planner\Goals\Goal;
 abstract class Badge_Maintenance extends Badge {
 
 	/**
+	 * The background color for the badge.
+	 *
+	 * @var string
+	 */
+	protected $background = 'var(--prpl-background-red)';
+
+	/**
 	 * Get a recurring goal for any type of weekly activity.
 	 *
 	 * @return \Progress_Planner\Goals\Goal_Recurring
@@ -71,14 +78,5 @@ abstract class Badge_Maintenance extends Badge {
 		}
 
 		return [];
-	}
-
-	/**
-	 * Get the background color for the badge.
-	 *
-	 * @return string
-	 */
-	public function get_background() {
-		return 'var(--prpl-background-red)';
 	}
 }
